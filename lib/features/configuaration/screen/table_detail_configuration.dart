92fe0a04817b3b949e67d57268753596d8c32adf
kooo12 <agkooo.ako36@gmail.com>
2024-05-23 19:38:37 +0630

    alittle changes for table detail view

--- lib/features/configuaration/screen/table_detail_configuration.dart
+++ lib/features/configuaration/screen/table_detail_configuration.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:restruant_pos/common/widgets/custom_shapes/containers/table_container.dart';
+import 'package:restruant_pos/features/configuaration/screen/table_grid_view.dart';
 import 'package:restruant_pos/features/configuaration/widgets/table_detail_appbar.dart';
 import 'package:restruant_pos/features/configuaration/controller/table_detail_controller.dart';
-import 'package:restruant_pos/utils/constant/enums.dart';
 import 'package:restruant_pos/utils/constant/sizes.dart';
 import 'package:restruant_pos/utils/constant/texts_strings.dart';
 
@@ -42,43 +41,6 @@ class TableDetailConfiguration extends StatelessWidget {
      
     );
   } 
-  Widget tableGridView (TableDetailsController controller){
-
-    int rowCount = controller.initialRow.value;
-    int columnCount = (controller.selectedFloor.length / rowCount).ceil();
-
-    List<Row> row = [];
-
-    for(int i = 0; i < rowCount; i++){
-      List<Widget> rowChildren = [];
-    
-    for(int j = 0; j < columnCount; j++){
-      int index = j * rowCount + i ;
-      if(index < controller.selectedFloor.length){
-        rowChildren.add(Padding(
-          padding: const EdgeInsets.all(TpsSizes.sm),
-          child: TableContainer(tableName: controller.selectedFloor[index]),
-        ));
-      }
-      else{
-        rowChildren.add(const Padding(
-          padding: EdgeInsets.all(TpsSizes.sm),
-          child: SizedBox(width: 150,height: 150,),
-        ));
-      }
-    }
-
-    if (controller.orderHorizontal.value == HorizontalOrder.rightToLeft){
-      rowChildren = rowChildren.reversed.toList();
-    }
-    row.add(Row(children: rowChildren,));
-    }
-
-    if(controller.orderVertical.value == VerticalOrder.bottomToTop){
-      row = row.reversed.toList();
-    }
-    return Column(children: row,);
-  }
 }
 
 
