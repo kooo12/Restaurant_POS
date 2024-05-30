967048576ce01a9c642487b94e1c90920ec3f755
kooo12 <agkooo.ako36@gmail.com>
2024-05-30 18:00:40 +0630

    changes for kitchen

--- lib/features/configuaration/kitchen_configuration/controllers/kitchen_order_controller.dart
+++ lib/features/configuaration/kitchen_configuration/controllers/kitchen_order_controller.dart
@@ -5,11 +5,85 @@ import 'package:restruant_pos/features/configuaration/model/table_model/table_mo
 
 class KitchenOrderController extends GetxController{
   final pendingOrders = [
-    OrderModel(id: 1, table: RTable(id: 2, name: 'T2'), menuItems: [MenuModel(id: 2, name: 'ငါးထမင်းနယ်', price: 5000, note: 'ငါးများများ ထည့်ပေးပါ',isUrgent: true),MenuModel(id: 15, name: 'American Fried Riced', price: 8000, note: null),MenuModel(id: 34, name: 'MoteHinNgar', price: 3000),MenuModel(id: 18, name: 'KyarZanHinn', price: 2500),MenuModel(id: 34, name: 'ArrPuSharPu', price: 1500)], isComplete: false),
-    OrderModel(id: 2, table: RTable(id: 8, name: 'T8'), menuItems: [MenuModel(id: 32, name: 'Tea', price: 2500, note: 'မလိုင်ထည့်ပေးပါ'),MenuModel(id: 12, name: 'Thai Style Fried Riced', price: 8000, note: 'အစပ်လျှော့')], isComplete: false)
+    OrderModel(id: 1, table: RTable(id: 2, name: 'T2'), menuItems: [MenuModel(id: 2, name: 'ငါးထမင်းနယ်', price: 5000, note: 'ငါးများများ ထည့်ပေးပါ',isUrgent: true),MenuModel(id: 3, name: 'American Fried Riced', price: 8000, note: null),MenuModel(id: 4, name: 'MoteHinNgar', price: 3000),MenuModel(id: 5, name: 'KyarZanHinn', price: 2500),MenuModel(id: 6, name: 'ArrPuSharPu', price: 1500)], isComplete: false),
+    OrderModel(id: 2, table: RTable(id: 8, name: 'T8'), menuItems: [MenuModel(id: 7, name: 'Tea', price: 2500, note: 'မလိုင်ထည့်ပေးပါ'),MenuModel(id: 8, name: 'Thai Style Fried Riced', price: 8000, note: 'အစပ်လျှော့')], isComplete: false),
+    OrderModel(id: 3, table: RTable(id: 5, name: 'T5'), menuItems: [MenuModel(id: 9, name: 'ငါးထမင်းနယ်', price: 5000, note: 'ငါးများများ ထည့်ပေးပါ',isUrgent: true),MenuModel(id: 10, name: 'American Fried Riced', price: 8000, note: null),MenuModel(id: 11, name: 'MoteHinNgar', price: 3000),MenuModel(id: 12, name: 'KyarZanHinn', price: 2500),MenuModel(id: 99, name: 'ArrPuSharPu', price: 1500)], isComplete: false),
+    OrderModel(id: 4, table: RTable(id: 6, name: 'T6'), menuItems: [MenuModel(id: 13, name: 'Tea', price: 2500, note: 'မလိုင်ထည့်ပေးပါ'),MenuModel(id: 14, name: 'Thai Style Fried Riced', price: 8000, note: 'အစပ်လျှော့')], isComplete: false),
+    OrderModel(id: 5, table: RTable(id: 16, name: 'T16'), menuItems: [MenuModel(id: 15, name: 'ငါးထမင်းနယ်', price: 5000, note: 'ငါးများများ ထည့်ပေးပါ',isUrgent: true),MenuModel(id: 16, name: 'American Fried Riced', price: 8000, note: null),MenuModel(id: 17, name: 'MoteHinNgar', price: 3000),MenuModel(id: 18, name: 'KyarZanHinn', price: 2500),MenuModel(id: 19, name: 'ArrPuSharPu', price: 1500)], isComplete: false),
+    OrderModel(id: 6, table: RTable(id: 20, name: 'T20'), menuItems: [MenuModel(id: 20, name: 'Tea', price: 2500, note: 'မလိုင်ထည့်ပေးပါ'),MenuModel(id: 21, name: 'Thai Style Fried Riced', price: 8000, note: 'အစပ်လျှော့')], isComplete: false),
+    OrderModel(id: 7, table: RTable(id: 30, name: 'T30'), menuItems: [MenuModel(id: 30, name: 'Tea', price: 2500, note: 'မလိုင်ထည့်ပေးပါ'),MenuModel(id: 31, name: 'Thai Style Fried Riced', price: 8000, note: 'အစပ်လျှော့')], isComplete: false),
+    OrderModel(id: 8, table: RTable(id: 33, name: 'T33'), menuItems: [MenuModel(id: 32, name: 'Tea', price: 2500, note: 'မလိုင်ထည့်ပေးပါ'),MenuModel(id: 33, name: 'Thai Style Fried Riced', price: 8000, note: 'အစပ်လျှော့')], isComplete: false)
   ].obs;
 
   final completedOrders = <OrderModel>[].obs;
+
+  final selectedMenuItems = <int>{}.obs;
+
+  void toggleSelection(int menuItemId){
+    if(selectedMenuItems.contains(menuItemId)){
+      selectedMenuItems.remove(menuItemId);
+    }
+    else{
+      selectedMenuItems.add(menuItemId);
+    }
+  }
+
+  List<MapEntry<MenuModel, int>> groupMenuItems(List<MenuModel> menuItems) {
+    final Map<String, int> groupedItems = {};
+    final Map<String, MenuModel> menuModelMap = {};
+
+    for (var menu in menuItems) {
+      final key = '${menu.id}_${menu.note ?? ''}';
+      if (groupedItems.containsKey(key)) {
+        groupedItems[key] = groupedItems[key]! + 1;
+      } else {
+        groupedItems[key] = 1;
+        menuModelMap[key] = menu;
+      }
+    }
+
+    return groupedItems.entries.map((e) => MapEntry(menuModelMap[e.key]!, e.value)).toList();
+  }
+
+  void moveSelectedMenusToComplete(){
+    final tempMap = <OrderModel,List<MenuModel>>{};
+    for (var order in pendingOrders){
+      for (var menu in order.menuItems.toList()){
+        if(selectedMenuItems.contains(menu.id)){
+          if(!tempMap.containsKey(order)){
+            tempMap[order] = [];
+          }
+          tempMap[order]!.add(menu);
+        }
+      }
+    }
+    tempMap.forEach((order, menuList) { 
+      for (var menu in menuList){
+        setMenuToComplete(order.id, menu.id);
+      }
+    });
+    selectedMenuItems.clear();
+  }
+
+  void moveSelectedMenusToIncomplete(){
+    final tempMap = <OrderModel,List<MenuModel>>{};
+    for (var order in completedOrders){
+      for (var menu in order.menuItems.toList()){
+        if(selectedMenuItems.contains(menu.id)){
+          if(!tempMap.containsKey(order)){
+            tempMap[order] = [];
+          }
+          tempMap[order]!.add(menu);
+        }
+      }
+    }
+    tempMap.forEach((order, menuList) { 
+      for (var menu in menuList){
+        setMenuToIncomplete(order.id, menu.id);
+      }
+    });
+    selectedMenuItems.clear();
+  }
 
   void setMenuToComplete(int orderId, int menuItemId) {
     var orderIndex = pendingOrders.indexWhere((order) => order.id == orderId);
--- lib/features/configuaration/kitchen_configuration/screens/kitchen1/widgets/order_list_view.dart
+++ lib/features/configuaration/kitchen_configuration/screens/kitchen1/widgets/order_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:restruant_pos/common/widgets/custom_shapes/containers/table_container.dart';
 import 'package:restruant_pos/features/configuaration/kitchen_configuration/controllers/kitchen_order_controller.dart';
@@ -10,7 +11,11 @@ class OrderListView extends StatelessWidget {
   const OrderListView({
     super.key,
     required this.controller,
-    required this.title, required this.orders, required this.isComplete, required this.tableTextStyle, required this.tableColor,
+    required this.title,
+    required this.orders,
+    required this.isComplete,
+    required this.tableTextStyle,
+    required this.tableColor,
   });
 
   final KitchenOrderController controller;
@@ -24,95 +29,166 @@ class OrderListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
-          title,
-          style: Theme.of(context).textTheme.headlineMedium,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Row(
+              children: [
+                isComplete
+                    ? SizedBox(
+                        width: 100,
+                        child: Obx(
+                          () => ElevatedButton(
+                              onPressed: controller.completedOrders.isEmpty
+                                  ? null
+                                  : () {},
+                              child: const Text('Ready')),
+                        ),
+                      )
+                    : const SizedBox(),
+                  const SizedBox(width: TpsSizes.md,),
+                SizedBox(
+                  width: 100,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        if (isComplete) {
+                          controller.moveSelectedMenusToIncomplete();
+                        } else {
+                          controller.moveSelectedMenusToComplete();
+                        }
+                      },
+                      child: Text(isComplete ? 'Restore' : 'Complete')),
+                ),
+              ],
+            ),
+          ],
         ),
-        const SizedBox(
-          height: TpsSizes.spaceBtwItems,
-        ),
-        Obx(
-          () => ListView.separated(
-            shrinkWrap: true,
-            itemCount: isComplete ? controller.completedOrders.length : controller.pendingOrders.length,
-            itemBuilder: (context, index) {
-              var order = orders[index];
-              var tableName = order.table.name;
-              var menuItems = order.menuItems;
-              if(menuItems.isEmpty){
-                return const SizedBox.shrink();
-              }
-              return Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TableContainer(
-                    tableName: Text(
-                      tableName,
-                      style: tableTextStyle,
-                    ),
-                    tableColor: tableColor,
-                    borderRadius: 10,
-                    width: 100,
-                    height: 60,
-                  ),
-                  const SizedBox(
-                    width: TpsSizes.spaceBtwSections,
-                  ),
-                  Expanded(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: menuItems.map((menu) {
-                
-                          return 
-                              
-                              GestureDetector(
-                                onTap: (){
-                                  if(isComplete){
-                                    controller.setMenuToIncomplete(order.id, menu.id);
-                                  }
-                                  else{
-                                    controller.setMenuToComplete(order.id, menu.id);
-                                  }
-                                },
-                                child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: TpsSizes.defaultSpace),
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      menu.name,
-                                      style: menu.isUrgent
-                                          ? Theme.of(context)
-                                              .textTheme
-                                              .titleLarge!
-                                              .copyWith(color: TpsColors.error)
-                                          : Theme.of(context).textTheme.titleLarge,
+        // const SizedBox(
+        //   height: TpsSizes.spaceBtwItems,
+        // ),
+        Expanded(
+          child: Obx(
+            () => Padding(
+              padding: const EdgeInsets.symmetric(vertical: TpsSizes.md),
+              child: ListView.separated(
+                shrinkWrap: true,
+                itemCount: orders.length,
+                itemBuilder: (context, index) {
+                  var order = orders[index];
+                  var tableName = order.table.name;
+                  var groupedMenuItems = controller.groupMenuItems(order.menuItems);
+                  // var hasSelected = menuItems.any((menu) => controller.selectedMenuItems.contains(menu.id));
+                  // if(menuItems.isEmpty){
+                  //   return const Text();
+                  // }
+                  return Obx(
+                    () => Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TableContainer(
+                          tableName: Text(
+                            tableName,
+                            style: tableTextStyle,
+                          ),
+                          tableColor: tableColor,
+                          borderRadius: 10,
+                          width: 100,
+                          height: 60,
+                        ),
+                        const SizedBox(
+                          width: TpsSizes.spaceBtwSections,
+                        ),
+                        Expanded(
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: groupedMenuItems.map((entry) {
+                                var menu = entry.key;
+                                var count = entry.value;
+                                bool isSelected = controller.selectedMenuItems
+                                    .contains(menu.id);
+                                return GestureDetector(
+                                  onTap: () {
+                                    controller.toggleSelection(menu.id);
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: TpsSizes.xs),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          color: isSelected
+                                              ? TpsColors.primary
+                                                  .withOpacity(0.3)
+                                              : null,
+                                          border: isSelected
+                                              ? Border.all(
+                                                  style: BorderStyle.none,
+                                                  color: TpsColors.primary,
+                                                  width: 2.0)
+                                              : null),
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: TpsSizes.sm,
+                                            vertical: TpsSizes.xs),
+                                        child: Column(
+                                          children: [
+                                            Text(
+                                              '${menu.name} x$count',
+                                              style: menu.isUrgent
+                                                  ? Theme.of(context)
+                                                      .textTheme
+                                                      .titleLarge!
+                                                      .copyWith(
+                                                          color:
+                                                              TpsColors.error)
+                                                  : Theme.of(context)
+                                                      .textTheme
+                                                      .titleLarge,
+                                            ),
+                                            menu.note != null
+                                                ? Text('(${menu.note})',
+                                                    style: Theme.of(context)
+                                                        .textTheme
+                                                        .titleSmall!
+                                                        .copyWith(
+                                                          color: TpsColors.blue,
+                                                        ))
+                                                : const SizedBox()
+                                          ],
+                                        ),
+                                      ),
                                     ),
-                                    menu.note != null
-                                        ? Text('(${menu.note})',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .titleSmall!
-                                                .copyWith(
-                                                  color: TpsColors.blue,
-                                                ))
-                                        : const SizedBox()
-                                  ],
-                                ),
-                                                            ),
-                              );
-                          
-                        }).toList(),
-                      ),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ),
+                        // IconButton(
+                        //     onPressed: () {
+                        // if (isComplete) {
+                        //   controller.moveSelectedMenusToIncomplete();
+                        // } else {
+                        //   controller.moveSelectedMenusToComplete();
+                        // }
+                        //     },
+                        //     icon: const Icon(
+                        //       Icons.check,
+                        //       color: Color.fromARGB(255, 30, 64, 8),
+                        //     ))
+                      ],
                     ),
-                  )
-                ],
-              );
-            },
-            separatorBuilder: (context, index) => const Divider(
-              height: 30,
+                  );
+                },
+                separatorBuilder: (context, index) => const Divider(
+                  height: 30,
+                ),
+              ),
             ),
           ),
         ),
